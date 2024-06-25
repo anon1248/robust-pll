20a328cab3ad097622b78e55e3b3c16d630c5791
Anon1248 <dgnr7vexskfoc3a67jdtf96kd9opz@protonmail.com>
2024-06-25 10:52:58 +0200

    Init repo

---
+++ partial_label_learning/adversarial.py
@@ -0,0 +1,28 @@
+""" Module to generate adversarial examples. """
+
+from typing import Callable
+
+import torch
+
+
+def generate_adversarial(
+    model_fn: Callable[[torch.Tensor], torch.Tensor],
+    inputs: torch.Tensor, eps: float,
+) -> torch.Tensor:
+    """ Use the fast-gradient-sign attack to generate adversarial examples. """
+
+    if eps > 0:
+        with torch.enable_grad():
+            # Compute loss
+            inputs = inputs.clone().detach().requires_grad_()
+            pred = torch.argmax(model_fn(inputs), dim=1)
+            loss_fn = torch.nn.CrossEntropyLoss()
+            loss = loss_fn(model_fn(inputs), pred)
+            loss.backward()
+
+        with torch.no_grad():
+            # Add adversarial noise
+            assert inputs.grad is not None
+            inputs += eps * torch.sign(inputs.grad)
+            inputs = torch.clip(inputs, 0, 1)
+    return inputs
